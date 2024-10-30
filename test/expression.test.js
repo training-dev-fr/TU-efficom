3af892fc8492fb8dac326577f3892869932ac7ac
training-dev <contact@training-dev.fr>
2024-10-30 18:11:55 +0100

    fix parse test

--- test/expression.test.js
+++ test/expression.test.js
@@ -1,15 +1,16 @@
 const {executeExpression} = require('./../src/parser');
-jest.mock('./../src/math', () => ({
-    sum: jest.fn(() => 25) 
-}));
+
 
 
 describe("When i try to parse an expression", () => {
     test("And the expression is an sum",() =>{
         let expression = "17+8";
-
+        jest.mock('./../src/math', () => ({
+            sum: jest.fn(() => 25)
+        }));
         const { sum } = require('./../src/math');
         let result = executeExpression(expression);
         expect(sum).toHaveBeenCalledTimes(1);
+        expect(result).toBe(25);
     })
 })
